a4ced34187001c3c714169949aea3ec4b3500048
William David <williamdavidalmeida36@gmail.com>
2020-08-11 10:05:19 -0400

    Commit Exercicios URI

---
+++ 1002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+
+    double pi = 3.14159;
+    double raio, area;
+
+    scanf("%f", &raio);
+    area = pi * (area*area);
+    printf("A=%f",area);
+    printf("\n");
+    return 0;
+}
