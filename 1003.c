a7d0d2d609588f6e56c7be252c3454ea9abcccd9
William David <williamdavidalmeida36@gmail.com>
2020-08-11 10:41:24 -0400

    exercicios 11.08.2020

---
+++ 1003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a, b, soma;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("SOMA = %d\n", soma = a+b);
+	return 0;
+	
+}
---
+++ 1004.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a,b, prod;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	printf("PROD = %d\n", prod = a* b);
+	return 0;
+}
